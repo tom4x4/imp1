349881516fff5f4f56a230ffc20ee1e83da358b2
tom <tom@4x4.czest.pl>
2021-03-26 14:21:10 +0100

    tables1

--- medicus/app1/tables.py
+++ medicus/app1/tables.py
@@ -1,9 +1,13 @@
 import django_tables2 as tables
+from django_tables2 import A
+import itertools
 
 from .models import Apteki
 
+
 class AptekiTable(tables.Table):
     selection = tables.CheckBoxColumn(accessor="pk")
+
     class Meta:
         model = Apteki
 
